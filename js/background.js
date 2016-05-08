9f279dbfe28c669e84adcb638dcc84d5b61a0291
Joey Smith <josephsmith@stmarksschool.org>
2016-05-08 14:05:16 -0400

    eee

--- js/background.js
+++ js/background.js
@@ -1,48 +1,93 @@
 // JavaScript Document
-var backgroundCol = new array(20);
-var changeColor = 0;
+var backgroundCol = new Array(53);
+var alphaVar = 0;
 
-backgroundCol[0] = "rgba(255,193,193,1.00)";
-backgroundCol[1] = "rgba(255,223,193,1.00)";
-backgroundCol[2] = "rgba(255,255,193,1.00)";
-backgroundCol[3] = "rgba(235,255,193,1.00)";
-backgroundCol[4] = "rgba(205,255,193,1.00)";
-backgroundCol[5] = "rgba(193,255,200,1.00)";
-backgroundCol[6] = "rgba(193,255,220,1.00)";
-backgroundCol[7] = "rgba(193,255,235,1.00)";
-backgroundCol[8] = "rgba(193,245,255,1.00)";
-backgroundCol[9] = "rgba(193,235,255,1.00)";
-backgroundCol[10] = "rgba(193,215,255,1.00)";
-backgroundCol[11] = "rgba(193,200,255,1.00)";
-backgroundCol[12] = "rgba(193,193,255,1.00)";
-backgroundCol[13] = "rgba(208,193,255,1.00)";
-backgroundCol[14] = "rgba(223,193,255,1.00)";
-backgroundCol[15] = "rgba(243,193,255,1.00)";
-backgroundCol[16] = "rgba(255,193,245,1.00)";
-backgroundCol[17] = "rgba(255,193,229,1.00)";
-backgroundCol[18] = "rgba(255,193,213,1.00)";
-backgroundCol[19] = "rgba(255,193,200,1.00)";
-backgroundCol[20] = "rgba(255,193,193,1.00)";
+backgroundCol[0] = 'rgba(255,192,192,';
+backgroundCol[1] = 'rgba(255,199,192,';
+backgroundCol[2] = 'rgba(255,206,192,';
+backgroundCol[3] = 'rgba(255,213,192,';
+backgroundCol[4] = 'rgba(255,220,192,';
+backgroundCol[5] = 'rgba(255,227,192,';
+backgroundCol[6] = 'rgba(255,234,192,';
+backgroundCol[7] = 'rgba(255,241,192,';
+backgroundCol[8] = 'rgba(255,249,192,';
+backgroundCol[9] = 'rgba(255,255,192,';
+backgroundCol[10] = 'rgba(248,255,192,';
+backgroundCol[11] = 'rgba(241,255,192,';
+backgroundCol[12] = 'rgba(234,255,192,';
+backgroundCol[13] = 'rgba(227,255,192,';
+backgroundCol[14] = 'rgba(220,255,192,';
+backgroundCol[15] = 'rgba(213,255,192,';
+backgroundCol[16] = 'rgba(206,255,192,';
+backgroundCol[17] = 'rgba(199,255,192,';
+backgroundCol[18] = 'rgba(192,255,192,';
+backgroundCol[19] = 'rgba(192,255,199,';
+backgroundCol[20] = 'rgba(192,255,206,';
+backgroundCol[21] = 'rgba(192,255,213,';
+backgroundCol[22] = 'rgba(192,255,220,';
+backgroundCol[23] = 'rgba(192,255,227,';
+backgroundCol[24] = 'rgba(192,255,234,';
+backgroundCol[25] = 'rgba(192,255,241,';
+backgroundCol[26] = 'rgba(192,255,249,';
+backgroundCol[27] = 'rgba(192,255,255,';
+backgroundCol[28] = 'rgba(192,248,255,';
+backgroundCol[29] = 'rgba(192,241,255,';
+backgroundCol[30] = 'rgba(192,234,255,';
+backgroundCol[31] = 'rgba(192,227,255,';
+backgroundCol[32] = 'rgba(192,220,255,';
+backgroundCol[33] = 'rgba(192,213,255,';
+backgroundCol[34] = 'rgba(192,206,255,';
+backgroundCol[35] = 'rgba(192,199,255,';
+backgroundCol[36] = 'rgba(192,192,255,';
+backgroundCol[37] = 'rgba(199,192,255,';
+backgroundCol[38] = 'rgba(206,192,255,';
+backgroundCol[39] = 'rgba(213,192,255,';
+backgroundCol[40] = 'rgba(220,192,255,';
+backgroundCol[41] = 'rgba(227,192,255,';
+backgroundCol[42] = 'rgba(235,192,255,';
+backgroundCol[43] = 'rgba(242,192,255,';
+backgroundCol[44] = 'rgba(249,192,255,';
+backgroundCol[45] = 'rgba(255,192,255,';
+backgroundCol[46] = 'rgba(255,192,248,';
+backgroundCol[47] = 'rgba(255,192,241,';
+backgroundCol[48] = 'rgba(255,192,234,';
+backgroundCol[49] = 'rgba(255,192,227,';
+backgroundCol[50] = 'rgba(255,192,220,';
+backgroundCol[51] = 'rgba(255,192,213,';
+backgroundCol[52] = 'rgba(255,192,206,';
+backgroundCol[53] = 'rgba(255,192,199,';
 
-window.onload = function() {
-	
-	setTimeout(myFunction, 3000)
-	
-	var backgroundColor = rgba(255,193,193,1.00);
-	
-	var changeColor = 0;
-	
-	document.body.style.backgroundColor = backgroundCol[changeColor];
-	
-	myFunction();
+var colorCounter = -1;
+var numberOfColors = 53;
+var colorTimer;
+var alphaTimer;
+var sinCounter = 0;
 
+function setColor(){
+	
+	$('body.page1').css('background-color', backgroundCol[colorCounter] + alphaVar + ')');
+	
+	colorCounter += 1;
+		if(colorCounter >= numberOfColors){
+		colorCounter = -1;
+		}
+		
 }
 
-function myFunction() {
-	changeColor++
-	if(changeColor < backgroundCol.length){
-		document.body.style.backgroundColor = background[changeColor];
-	} else {
-		changeColor =0;
+function manageAlpha() {
+    sinCounter = sinCounter + Math.PI / 30.5;
+    alphaVar = Math.abs(Math.sin(sinCounter));
+	
+	if (Math.abs(Math.sin(sinCounter))<=0.15) {
+		alphaVar = (Math.abs(Math.sin(sinCounter)) + .075)
 	}
 }
+
+$(document).ready(function(){	
+
+	$('body.page1').css('background-color', backgroundCol[colorCounter] + alphaVar + ')');
+	
+	colorTimer = setInterval(setColor, 600);
+	alphaTimer = setInterval(manageAlpha, 20);
+
+});
--- js/myScript.js
+++ js/myScript.js
@@ -1,45 +1,36 @@
-var picList = new Array (2);
-
-var bButtonListener;
-var fButtonListener;
-
-picList[0] = "images/appleparkman.png";
-picList[1] = "images/wmts.png";
-
-var whatPicture;
-
-window.onload = function() {
-
-var whatPicture = 0;
+/*$(document).ready(function(){
 	
-	document.getElementById("pictureView").src = picList[whatPicture];
-	
-	fButtonListener = document.getElementById("")
-	bButtonListener = document.getElementById("")
-	
-	fButtonListener.addEventListener('click', function(event) {
-		whatPicture++
-		whatCaption++
-		whatLocation++
-		if(whatPicture >= picList.length - 1) {
-		whatPicture = 0;
-		whatCaption = 0;
-		whatLocation = 0;
-	}
-	document.getElementById("pictureView").src = picList[whatPicture];
-	
-	});
-	
-	bButtonListener.addEventListener('click', function(event) {
-		whatPicture--
-		whatCaption--
-		whatLocation--
-	if(whatPicture < 0) {
-		whatPicture = picList.length - 1;
-		whatCaption = captionData.length - 1;
-		whatLocation = locationData.length - 1;
-	}
-	document.getElementById("pictureView").src = picList[whatPicture];
-	});
+	$('span').css ('display', 'none');
+	 $("img").fadeTo(100, 0.2, function(){});
+	 $("img").rotate(-45);
+	 $('span').fadeTo(50, 0, function(){});
 
-}
+	$("#leftSide").hover(function () { 
+			   $('#leftSide').animate({'width': '30%'}, { duration: 1000, queue: false });
+			   $('#rightSide').animate({'width': '69%'}, { duration: 1000, queue: false,
+			   	complete: function(){
+			   		$('span').css ('display', 'inline');
+			   		$("img").fadeTo(1000, 1.0, function(){});
+			   		$('span').fadeTo(1000, 1.0, function(){});
+			   		$("img").rotate({duration: 1000, animateTo: 0 });
+			   	}
+			   	});
+    //$('span').animate({"font-color" : "#FAF93C"}, { duration: 1000, queue: false });
+					   
+// colors : FAF93C 5A79A5
+
+            }, function () {
+               $('#leftSide').animate({'width': '64px'}, 1000);
+			   $('#rightSide').animate({'width': '92%'}, 1000);
+			   $('span').css ('display', 'none');
+			   $("img").fadeTo(1000, 0.2, function(){});
+			   $('span').fadeTo(1000, 0, function(){});
+			   $("img").rotate({
+     				 duration: 1000,
+      				 animateTo: -45 });
+
+            });
+
+
+
+});*/
