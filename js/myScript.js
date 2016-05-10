9979f785ba7cd8e3393621639427b12ddefb8042
Joey Smith <josephsmith@stmarksschool.org>
2016-05-10 09:42:09 -0400

    third

--- js/myScript.js
+++ js/myScript.js
@@ -1,13 +1,13 @@
-/*$(document).ready(function(){
+$(document).ready(function(){
 	
 	$('span').css ('display', 'none');
 	 $("img").fadeTo(100, 0.2, function(){});
 	 $("img").rotate(-45);
 	 $('span').fadeTo(50, 0, function(){});
 
-	$("#leftSide").hover(function () { 
-			   $('#leftSide').animate({'width': '30%'}, { duration: 1000, queue: false });
-			   $('#rightSide').animate({'width': '69%'}, { duration: 1000, queue: false,
+	$("ul").hover(function () { 
+			   $('ul').animate({'height': '30%'}, { duration: 1000, queue: false });
+			   //$('#rightSide').animate({'width': '69%'}, { duration: 1000, queue: false,
 			   	complete: function(){
 			   		$('span').css ('display', 'inline');
 			   		$("img").fadeTo(1000, 1.0, function(){});
@@ -15,13 +15,13 @@
 			   		$("img").rotate({duration: 1000, animateTo: 0 });
 			   	}
 			   	});
-    //$('span').animate({"font-color" : "#FAF93C"}, { duration: 1000, queue: false });
+    $('span').animate({"font-color" : "#FAF93C"}, { duration: 1000, queue: false });
 					   
 // colors : FAF93C 5A79A5
 
-            }, function () {
-               $('#leftSide').animate({'width': '64px'}, 1000);
-			   $('#rightSide').animate({'width': '92%'}, 1000);
+			}, function () {
+               $('ul').animate({'height': '64px'}, 1000);
+			  // $('#rightSide').animate({'width': '92%'}, 1000);
 			   $('span').css ('display', 'none');
 			   $("img").fadeTo(1000, 0.2, function(){});
 			   $('span').fadeTo(1000, 0, function(){});
@@ -33,4 +33,4 @@
 
 
 
-});*/
+//});
